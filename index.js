74d4acdc24420b7409fb51137849e4f90022f5c4
haryeloliveira01-dotcom <haryeloliveira01@gmail.com>
2026-03-30 09:28:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.get("/player/:tag", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Servidor rodando");
 });
